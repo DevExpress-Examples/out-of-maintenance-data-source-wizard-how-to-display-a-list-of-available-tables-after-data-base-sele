7553cb4df1e693e3603fcb06612ae7f8a34bbe47
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DSWizardQueryTables/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports DevExpress.XtraBars.Ribbon
+
+Namespace DSWizardCustomizationNoSP
+    Partial Public Class Form1
+        Inherits RibbonForm
+
+        Public Sub New()
+            InitializeComponent()
+            dashboardDesigner1.DataSourceWizardCustomization = New DataSourceWizardCustomization()
+        End Sub
+    End Class
+End Namespace
